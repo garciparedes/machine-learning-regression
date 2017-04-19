88250de637dfbd16222b0f5bab0e78dd348b2b8b
Sergio Garcia <sergio@garciparedes.me>
2017-04-19 09:39:29 +0200

    Improved Error calculation

--- weka/results/weka_relative_error.py
+++ weka/results/weka_relative_error.py
@@ -6,7 +6,9 @@ import pandas as pd
 
 if __name__ == "__main__":
     file_name = sys.argv[1]
-    error_tase = float(sys.argv[2])
+    error_ratio_list = [float(i) for i in sys.argv[2:]]
     file_error = pd.read_csv(file_name)
-    error_count = (file_error['error'].abs() / file_error['actual']<= error_tase).sum()
-    print( error_count / float(file_error.shape[0]))
+
+    for error_ratio in error_ratio_list:
+        error_count = (file_error['error'].abs() / file_error['actual']<= error_ratio).sum()
+        print('Max Rel. Error of ' + str(error_ratio) + '\t-> Error Ratio:\t'  + str(error_count / float(file_error.shape[0])))
