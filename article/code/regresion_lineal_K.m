f0a38ee7ac42b59bb297081e7c33d35c6dd90cde
Sergio Garcia <sergio@garciparedes.me>
2017-05-10 10:50:33 +0200

    Added code for aticle

---
+++ article/code/regresion_lineal_K.m
@@ -0,0 +1,11 @@
+function w = regresion_lineal_K(x, y)
+  A = zeros(size(x)(2), size(x)(2));
+  B = zeros(size(x)(2), 1);
+  for i=1:size(x)(2)
+    for j=1:size(x)(2)
+      A(i,j)=sum(x(:,i).*x(:,j));
+    end
+    B(i,1)=sum(y.*x(:,i));
+  end
+  w=inv(A)*B;
+end
---
+++ article/code/regresion_logistica_K.m
@@ -0,0 +1,6 @@
+function w = regresion_logistica_K(x, y)
+  w = zeros( size(x,2),1);
+  nu = 1 ./ (1 .+ e.^-(x * w));
+  s = (nu .* (1 .- nu));
+  w = inv(x' .* s' * x)*x'*(s'*x*w + y - nu);
+end
