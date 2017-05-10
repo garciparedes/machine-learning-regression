fb638a61f8cadf850d9826fe4d65dcf3627fb80f
Sergio Garcia <sergio@garciparedes.me>
2017-05-10 10:50:03 +0200

    Finishing article

--- src/regresion_logistica_K.m
+++ src/regresion_logistica_K.m
@@ -7,8 +7,6 @@
 
 function w = regresion_logistica_K(x, y)
   w = zeros( size(x,2),1);
-  nu = zeros(size(x,2),1);
-  s = zeros(size(x,1),1);
   nu = 1 ./ (1 .+ e.^-(x * w));
   s = (nu .* (1 .- nu));
   w = inv(x' .* s' * x)*x'*(s'*x*w + y - nu);
